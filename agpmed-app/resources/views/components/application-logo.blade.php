894db5a9eedc273e19d879e1f345a9cc4b8ebdbb
ricardosimasjr <ricardosimasjr@gmail.com>
2024-06-05 20:25:05 -0300

    acerto local assets

--- agpmed-app/resources/views/components/application-logo.blade.php
+++ agpmed-app/resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img style="width: 300px; margin-left: auto; margin-right: auto" src="{{ Vite::asset('resources/images/logo-login.png')}}" alt="">
+<img style="width: 300px; margin-left: auto; margin-right: auto" src="{{asset('images/logo-login.png')}}" alt="">
