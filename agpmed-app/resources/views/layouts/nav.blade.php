f2e089d10972ffe9017ddb682b41546235f2b780
ricardosimasjr <ricardosimasjr@gmail.com>
2024-08-27 16:30:40 -0300

    Correção NavBar

--- agpmed-app/resources/views/layouts/nav.blade.php
+++ agpmed-app/resources/views/layouts/nav.blade.php
@@ -1,43 +1,35 @@
 <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-            <img src="{{ Vite::asset('resources/images/ico-agpmed.png')}}" width="60" >
-          </a>
-
-        <div class="navbar-collapse collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mb-lg-0 mb-2 me-auto">
+        <a class="navbar-brand" href="{{ route('home') }}">
+            <img src="{{ Vite::asset('resources/images/ico-agpmed.png') }}" width="60">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll"
+            aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarScroll">
+            <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Cadastros
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        Cadastros
                     </a>
                     <ul class="dropdown-menu">
-                      <li><a class="dropdown-item" href="{{ route('transportador.list')}}">Transportadoras</a></li>
-                      <li><a class="dropdown-item" href="#">Status</a></li>
-                      <li><hr class="dropdown-divider"></li>
-                      <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <li><a class="dropdown-item" href="{{ route('transportador.list') }}">Transportadoras</a></li>
+                        <li><a class="dropdown-item disabled" href="#">Status</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item disabled" href="#">Configurações</a></li>
                     </ul>
-                  </li>
+                </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('pedidos.list') }}">Pedidos</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('notas.list') }}">Notas</a>
+                    <a class="nav-link disabled" href="{{ route('notas.list') }}">Notas</a>
                 </li>
             </ul>
-            <div class="d-flex">
-                <ul class="navbar-nav mb-lg-0 mb-2 me-auto">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                          {{ Auth::user()->name}}
-                        </a>
-                        <ul class="dropdown-menu">
-                          <li><a class="dropdown-item" href="{{ route('profile.edit')}}">Profile</a></li>
-                          <li><hr class="dropdown-divider"></li>
-                          <li><a class="dropdown-item" href="{{route('user.logout')}}">Logout</a></li>
-                        </ul>
-                      </li>
-                </ul>
-            </div>
         </div>
     </div>
 </nav>
