4f82bc41f8f43dec74858a248ec0b26d568627d5
ricardosimasjr <ricardosimasjr@gmail.com>
2024-11-29 13:03:39 -0300

    Romaneio

---
+++ agpmed-app/app/Models/Romaneio.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Romaneio extends Model
+{
+    use HasFactory;
+}
